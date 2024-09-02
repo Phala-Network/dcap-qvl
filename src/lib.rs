ca114edb6150ab596ed2ad34e611c92017ee1b71
Kevin Wang <wy721@qq.com>
2024-09-02 05:41:52 +0000

    Implement tdx quote verification

---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+#![cfg_attr(all(not(test), not(feature = "std")), no_std)]
+
+#[macro_use]
+extern crate alloc;
+
+use scale::{Decode, Encode};
+use scale_info::TypeInfo;
+
+#[derive(Encode, Decode, TypeInfo, Debug, Clone, PartialEq, Eq)]
+pub enum Error {
+    InvalidCertificate,
+    InvalidSignature,
+    CodecError,
+
+    // DCAP
+    TCBInfoExpired,
+    KeyLengthIsInvalid,
+    PublicKeyIsInvalid,
+    RsaSignatureIsInvalid,
+    DerEncodingError,
+    UnsupportedDCAPQuoteVersion,
+    UnsupportedDCAPAttestationKeyType,
+    UnsupportedQuoteAuthData,
+    UnsupportedDCAPPckCertFormat,
+    LeafCertificateParsingError,
+    CertificateChainIsInvalid,
+    CertificateChainIsTooShort,
+    IntelExtensionCertificateDecodingError,
+    IntelExtensionAmbiguity,
+    CpuSvnLengthMismatch,
+    CpuSvnDecodingError,
+    PceSvnDecodingError,
+    PceSvnLengthMismatch,
+    FmspcLengthMismatch,
+    FmspcDecodingError,
+    FmspcMismatch,
+    QEReportHashMismatch,
+    IsvEnclaveReportSignatureIsInvalid,
+    DerDecodingError,
+    OidIsMissing,
+}
+
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug)]
+pub struct QuoteCollateralV3 {
+    pub pck_crl_issuer_chain: String,
+    pub root_ca_crl: String,
+    pub pck_crl: String,
+    pub tcb_info_issuer_chain: String,
+    pub tcb_info: String,
+    pub tcb_info_signature: Vec<u8>,
+    pub qe_identity_issuer_chain: String,
+    pub qe_identity: String,
+    pub qe_identity_signature: Vec<u8>,
+}
+
+#[cfg(feature = "report")]
+pub mod collateral;
+
+mod constants;
+mod tcb_info;
+mod utils;
+
+pub mod quote;
+pub mod verify;
