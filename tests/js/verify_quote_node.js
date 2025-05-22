08707ef9d113ae2c271954a9e1b623b96d65ad98
Shawn TIAN <hightian@gmail.com>
2025-05-22 19:23:34 +0800

    add get_collateral and fix the old verify_quote

--- tests/js/verify_quote_node.js
+++ tests/js/verify_quote_node.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const { js_verify } = require('../../pkg/node/dcap-qvl-node');
+const fs = require("fs");
+const path = require("path");
+const { js_verify } = require("../../pkg/node/dcap-qvl-node");
 
 // Function to read a file as a Uint8Array
 function readFileAsUint8Array(filePath) {
@@ -8,23 +8,31 @@ function readFileAsUint8Array(filePath) {
     return new Uint8Array(data);
 }
 
+function readFileAsString(filePath) {
+    return fs.readFileSync(filePath, "utf8");
+}
+
 // Paths to your sample files
-const rawQuotePath = path.join(__dirname, '../../sample', 'tdx_quote');
-const quoteCollateralPath = path.join(__dirname, '../../sample', 'tdx_quote_collateral');
+const rawQuotePath = path.join(__dirname, "../../sample", "tdx_quote");
+const quoteCollateralPath = path.join(
+    __dirname,
+    "../../sample",
+    "tdx_quote_collateral.json"
+);
 
 // Read the files
 const rawQuote = readFileAsUint8Array(rawQuotePath);
-const quoteCollateral = readFileAsUint8Array(quoteCollateralPath);
+const quoteCollateral = readFileAsString(quoteCollateralPath);
 
 // Current timestamp
 // TCBInfoExpired when using current timestamp, pick the time from verify_quote.rs
 // const now = BigInt(Math.floor(Date.now() / 1000));
-const now = BigInt(1725258675);
+const now = BigInt(1741852249);
 
 try {
     // Call the js_verify function
     const result = js_verify(rawQuote, quoteCollateral, now);
-    console.log('Verification Result:', result);
+    console.log("Verification Result:", result);
 } catch (error) {
-    console.error('Verification failed:', error);
+    console.error("Verification failed:", error);
 }
--- tests/js/verify_quote_web.js
+++ tests/js/verify_quote_web.js
@@ -1,4 +1,4 @@
-import init, { js_verify } from '/pkg/web/dcap-qvl-web.js';
+import init, { js_verify } from "/pkg/web/dcap-qvl-web.js";
 
 // Function to fetch a file as a Uint8Array
 async function fetchFileAsUint8Array(url) {
@@ -7,30 +7,33 @@ async function fetchFileAsUint8Array(url) {
     return new Uint8Array(data);
 }
 
+function fetchFileAsString(filePath) {
+    return fetch(filePath).then((response) => response.text());
+}
+
 // URLs to your sample files
-const rawQuoteUrl = '/sample/tdx_quote';
-const quoteCollateralUrl = '/sample/tdx_quote_collateral';
+const rawQuoteUrl = "/sample/tdx_quote";
+const quoteCollateralUrl = "/sample/tdx_quote_collateral.json";
 
 // Load the files
 async function loadFilesAndVerify() {
     try {
         // Initialize the WASM module
-        await init('/pkg/web/dcap-qvl-web_bg.wasm');
+        await init("/pkg/web/dcap-qvl-web_bg.wasm");
 
         const rawQuote = await fetchFileAsUint8Array(rawQuoteUrl);
-        const quoteCollateral = await fetchFileAsUint8Array(quoteCollateralUrl);
+        const quoteCollateral = await fetchFileAsString(quoteCollateralUrl);
 
         // Current timestamp
-        const now = BigInt(1725258675);
+        const now = BigInt(1741852249);
 
         // Call the js_verify function
         const result = js_verify(rawQuote, quoteCollateral, now);
-        console.log('Verification Result:', result);
+        console.log("Verification Result:", result);
     } catch (error) {
-        console.error('Verification failed:', error);
+        console.error("Verification failed:", error);
     }
 }
 
 // Execute the verification
 loadFilesAndVerify();
-
