db2f7c68fc6005b5fbbd4d4789a73573c3f9e4e5
Shawn TIAN <hightian@gmail.com>
2025-05-23 12:41:24 +0800

    refactor not using JSON.stringify in verify

--- tests/js/verify_quote_node.js
+++ tests/js/verify_quote_node.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 const {
-    js_verify,
-    js_get_collateral,
+  js_verify,
+  js_get_collateral,
 } = require("../../pkg/node/dcap-qvl-node");
 
 // Function to read a file as a Uint8Array
 function readFileAsUint8Array(filePath) {
-    const data = fs.readFileSync(filePath);
-    return new Uint8Array(data);
+  const data = fs.readFileSync(filePath);
+  return new Uint8Array(data);
 }
 
 // Paths to your sample files
@@ -21,17 +21,13 @@ const rawQuote = readFileAsUint8Array(rawQuotePath);
 const now = BigInt(Math.floor(Date.now() / 1000));
 
 (async () => {
-    try {
-        // Call the js_verify function
-        let pccs_url = "https://pccs.phala.network/tdx/certification/v4";
-        const quoteCollateral = await js_get_collateral(pccs_url, rawQuote);
-        const result = js_verify(
-            rawQuote,
-            JSON.stringify(quoteCollateral),
-            now
-        );
-        console.log("Verification Result:", result);
-    } catch (error) {
-        console.error("Verification failed:", error);
-    }
+  try {
+    // Call the js_verify function
+    let pccs_url = "https://pccs.phala.network/tdx/certification/v4";
+    const quoteCollateral = await js_get_collateral(pccs_url, rawQuote);
+    const result = js_verify(rawQuote, quoteCollateral, now);
+    console.log("Verification Result:", result);
+  } catch (error) {
+    console.error("Verification failed:", error);
+  }
 })();
--- tests/js/verify_quote_web.js
+++ tests/js/verify_quote_web.js
@@ -2,13 +2,13 @@ import init, { js_verify, js_get_collateral } from "/pkg/web/dcap-qvl-web.js";
 
 // Function to fetch a file as a Uint8Array
 async function fetchFileAsUint8Array(url) {
-    const response = await fetch(url);
-    const data = await response.arrayBuffer();
-    return new Uint8Array(data);
+  const response = await fetch(url);
+  const data = await response.arrayBuffer();
+  return new Uint8Array(data);
 }
 
 function fetchFileAsString(filePath) {
-    return fetch(filePath).then((response) => response.text());
+  return fetch(filePath).then((response) => response.text());
 }
 
 // URLs to your sample files
@@ -17,30 +17,26 @@ const quoteCollateralUrl = "/sample/tdx_quote_collateral.json";
 
 // Load the files
 async function loadFilesAndVerify() {
-    try {
-        // Initialize the WASM module
-        await init("/pkg/web/dcap-qvl-web_bg.wasm");
+  try {
+    // Initialize the WASM module
+    await init("/pkg/web/dcap-qvl-web_bg.wasm");
 
-        // Get the raw quote from local file
-        const rawQuote = await fetchFileAsUint8Array(rawQuoteUrl);
+    // Get the raw quote from local file
+    const rawQuote = await fetchFileAsUint8Array(rawQuoteUrl);
 
-        // Get the quote collateral
-        let pccs_url = "https://pccs.phala.network/tdx/certification/v4";
-        const quoteCollateral = await js_get_collateral(pccs_url, rawQuote);
+    // Get the quote collateral
+    let pccs_url = "https://pccs.phala.network/tdx/certification/v4";
+    const quoteCollateral = await js_get_collateral(pccs_url, rawQuote);
 
-        // Current timestamp
-        const now = BigInt(Math.floor(Date.now() / 1000));
+    // Current timestamp
+    const now = BigInt(Math.floor(Date.now() / 1000));
 
-        // Call the js_verify function
-        const result = js_verify(
-            rawQuote,
-            JSON.stringify(quoteCollateral),
-            now
-        );
-        console.log("Verification Result:", result);
-    } catch (error) {
-        console.error("Verification failed:", error);
-    }
+    // Call the js_verify function
+    const result = js_verify(rawQuote, quoteCollateral, now);
+    console.log("Verification Result:", result);
+  } catch (error) {
+    console.error("Verification failed:", error);
+  }
 }
 
 // Execute the verification
