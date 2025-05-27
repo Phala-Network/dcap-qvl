e937f5c4f4a357599c378839a8968702bd89e65a
Shawn TIAN <hightian@gmail.com>
2025-05-27 18:32:41 +0800

    remove wasm manually import

--- tests/esbuild/src/main.ts
+++ tests/esbuild/src/main.ts
@@ -1,5 +1,4 @@
 import init, { js_verify, js_get_collateral } from "@phala/dcap-qvl-web";
-import wasm from "@phala/dcap-qvl-web/dcap-qvl-web_bg.wasm";
 
 const PCCS_URL = "https://pccs.phala.network/tdx/certification/v4";
 
@@ -12,7 +11,7 @@ async function fetchQuoteAsUint8Array(url: string): Promise<Uint8Array> {
   return new Uint8Array(arrayBuffer);
 }
 
-init(wasm).then(() => {
+init().then(() => {
   console.log("Phala DCAP QVL initialized!");
   // You can now use js_verify, js_get_collateral, etc.
   fetchQuoteAsUint8Array("/sample/tdx_quote").then(async (rawQuote) => {
