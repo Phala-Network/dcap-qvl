49e84afbab88dab0678c35e6681ae7dc95be3091
maradini77 <140460067+maradini77@users.noreply.github.com>
2026-01-26 14:43:30 +0100

    Update quote.rs

--- src/quote.rs
+++ src/quote.rs
@@ -504,10 +504,10 @@ impl Decode for Quote {
     fn decode<I: Input>(input: &mut I) -> Result<Self, scale::Error> {
         let header = Header::decode(input)?;
         let report;
-        match header.version {
+            match header.version {
             3 => {
                 if header.tee_type != TEE_TYPE_SGX {
-                    return Err(scale::Error::from("invalid tee type"));
+                    return Err(scale::Error::from("Invalid TEE type"));
                 }
                 report = Report::SgxEnclave(EnclaveReport::decode(input)?);
             }
